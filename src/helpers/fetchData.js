8e5dc5c9b2201bf1ea276763b9235604a2496a9d
himanshu <himanshu.dwivedi@externa.swiggy.in>
2023-10-08 16:00:14 +0530

    fix: headers

--- src/helpers/fetchData.js
+++ src/helpers/fetchData.js
@@ -5,5 +5,10 @@ export const getUsers = (userName, endPoint, shouldSort) => {
 		? `${baseUrl}/${endPoint}?q=${userName}&sort=followers`
 		: `${baseUrl}/${endPoint}/${userName}`;
 
-	return fetch(formattedUrl);
+	return fetch(formattedUrl, {
+		headers: {
+			Accept: "pplication/vnd.github+json",
+			Authorization: `Bearer ${process.env.REACT_APP_SECRET_TOKEN}`,
+		},
+	});
 };
