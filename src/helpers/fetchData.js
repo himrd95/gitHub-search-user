5e01791415ba7972b92600b2534e27c00d1039b3
himanshu <himanshu.dwivedi@externa.swiggy.in>
2023-10-08 15:47:18 +0530

    added deploy script

--- src/helpers/fetchData.js
+++ src/helpers/fetchData.js
@@ -5,10 +5,5 @@ export const getUsers = (userName, endPoint, shouldSort) => {
 		? `${baseUrl}/${endPoint}?q=${userName}&sort=followers`
 		: `${baseUrl}/${endPoint}/${userName}`;
 
-	return fetch(formattedUrl, {
-		headers: {
-			Accept: "pplication/vnd.github+json",
-			Authorization: `Bearer ${process.env.REACT_APP_SECRET_TOKEN}`,
-		},
-	});
+	return fetch(formattedUrl);
 };
